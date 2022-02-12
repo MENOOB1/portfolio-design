11b1178635e80004e9295fbe895742d6fe52202f
rohitverma1222 <8810455235@gmail.com>
2022-02-12 16:03:09 +0530

    added animation

---
+++ fun.js
@@ -0,0 +1,24 @@
+
+
+let social=document.querySelector(".fir");
+social.addEventListener("mouseenter",function(){
+    gsap.from(".fir",{ rotation :360, duration:0.3})
+
+})
+
+let social1=document.querySelector(".sec");
+social1.addEventListener("mouseenter",function(){
+    gsap.from(".sec",{ rotation :360, duration:0.3})
+
+})
+let social2=document.querySelector(".third");
+social2.addEventListener("mouseenter",function(){
+    gsap.from(".third",{ rotation :360, duration:0.3})
+
+})
+
+
+var t1=gsap.timeline();
+t1.from('.nav-bar',{duration:2,x:'-100%' ,ease:'elastic'})
+t1.to('.get-cv-btn',{y:'100%', ease:'bounce'})
+gsap.from(".social-links",{duration:2,y:-1000})
